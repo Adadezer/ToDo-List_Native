d266bf42d57dd6410c7bfa27dd59dc81e08c0b8a
Adadezer <adadezer@gmail.com>
2026-03-06 16:52:22 -0300

    feat: create navigation beetwen pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { Text, Button, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Ir para Detalhes"
+        onPress={() => navigation.navigate('Detalhes')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Voltar para a página inicial"
+        onPress={() => navigation.goBack()}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <SafeAreaView>
-        <Text style={{ fontSize: 40}}>
-          Olá Pessoal do FSC
-        </Text>
-      </SafeAreaView>
+
+      <Stack.Navigator>
+        <Stack.Screen name="Inicio" component={HomeScreen} />
+        <Stack.Screen name="Detalhes" component={DetailsScreen} options={{headerShown: false}} />
+      </Stack.Navigator>
+    
+      {/* <Text style={{ fontSize: 40}}>
+        Olá Pessoal do FSC teste
+      </Text>
+      <Button title="Botão legal da aula 2" onPress={() => alert('Botão clicado!')} /> */}
+      
     </NavigationContainer>
   );
 }
