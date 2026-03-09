9bf5be643b69a11d6c30d33db1f539be9a606354
Adadezer <adadezer@gmail.com>
2026-03-09 12:26:21 -0300

    feat: change layout of initial message, create list of activities in Home screen, and change layout in Details screen

--- src/screens/Details/index.js
+++ src/screens/Details/index.js
@@ -1,13 +1,40 @@
 import { Button, Text, View } from "react-native";
 
-function Details({navigation}) {
+function Details({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details teste componente separado</Text>
-      <Button
-        title="Voltar para a página inicial"
-        onPress={() => navigation.goBack()}
-      />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      
+      <View
+        style={{
+          margin: 10,
+          backgroundColor: 'rgba(0, 225, 255, 0.3)',
+          padding: 20,
+          alignItems: 'center',
+          gap: 10,
+          borderRadius: 10,
+          width: '85%'
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 16,
+            fontStyle: 'italic',
+            textAlign: 'center',
+            color: 'rgb(1, 116, 106)'
+          }}
+        >
+          Você tem sido o melhor dos meus dias, amo suas mensagens, seus áudios,
+          seus vídeos, sua companhia, seu carinho... Já estou com saudades de você
+        </Text>
+
+        <View style={{ marginTop: 20 }}>
+          <Button
+            title="Voltar para a página inicial"
+            onPress={() => navigation.goBack()}
+          />
+        </View>
+
+      </View>
     </View>
   );
 }
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,13 +1,68 @@
-import { Button, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, FlatList, Text, View } from "react-native";
+
+function ItemList({ item }) {
+  const backgroundColor = item.done
+    ? 'rgba(0,180,0,0.8)'
+    : 'rgba(255,0,0,0.7)';
+
+  return (
+    <View
+      style={{
+        backgroundColor,
+        padding: 20,
+        marginVertical: 8,
+      }}
+    >
+      <Text>{item.name}</Text>
+    </View>
+  );
+}
+
+function EmptyList() {
+  return (
+    <View style={{ alignItems: 'center', gap: 10 }}>
+      <Text>Adicione uma atividade para começar</Text>
+    </View>
+  );
+}
 
 function Home({navigation}) {
+
+  const [items, setItems] = useState([]);
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home teste</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', marginBottom: 30 }}>
+      <View
+        style={{
+          marginTop: 10,
+          marginBottom: 20,
+          backgroundColor: 'rgba(0, 225, 255, 0.3)',
+          padding: 20,
+          alignItems: 'center',
+          gap: 5,
+          borderRadius: 10
+
+        }}
+      >
+
+      <Text style={{color: 'rgb(6, 185, 170)', fontWeight: 'bold', fontSize: 15}}>Olá, minha princesa</Text>
+      
       <Button
-        title="Ir para Detalhes"
+        title="Clique para ver o recado"
         onPress={() => navigation.navigate('Detalhes')}
       />
+      </View>
+        
+      <FlatList data={items} renderItem={ItemList} ListEmptyComponent={EmptyList} />
+
+      <View style={{marginBottom: 20}}>
+        <Button
+          title="Clique para adicionar uma atividade"
+          onPress={() => setItems([...items, { name: `item ${items.length + 1}`, done: false }])}
+          />
+      </View>
+
     </View>
   );
 }
