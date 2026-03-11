7f072f1977dd58325cc2c282b27e5c2367558346
Adadezer <adadezer@gmail.com>
2026-03-10 21:50:26 -0300

    feat: create "CRUD" of To Do list and refactor layout of the items

--- src/screens/Home/components/ItemList/index.js
+++ src/screens/Home/components/ItemList/index.js
@@ -1,20 +1,48 @@
-import { Text, View } from "react-native";
-function ItemList({ item }) {
-  const backgroundColor = item.done
-    ? 'rgba(4, 131, 131, 0.7)'
-    : 'rgba(90, 196, 196, 0.7)';
+import { Text, View, Pressable, Alert } from "react-native";
+import { stylesItemList } from "./styles";
+import { Pencil, Trash2, CircleCheckBig } from "lucide-react-native";
+import { useNavigation } from "@react-navigation/native";
+
+function ActionButton({ onPress, icon }) {
+  return (
+    <Pressable onPress={onPress} style={{ padding: 8 }}>
+      {icon === "edit" && <Pencil size={24} color="#004d96" />}
+      {icon === "delete" && <Trash2 size={24} color="#004d96" />}
+      {icon === "check-circle" && <CircleCheckBig size={24} color="#004d96" />}
+    </Pressable>
+  );
+}
+function ItemList({ item, index, completeItemPress, removeItemPress }) {
+
+  const stylesList = stylesItemList(item.done);
+
+  const navigation = useNavigation();
+
+  const handleClickEdit = () => {
+    // Alert.alert("O botão Editar foi clicado, funcionou!");
+    navigation.navigate("Editar", { item });
+  }
+
+  const handleClickDelete = () => {
+    removeItemPress({item, index});
+  }
+  const handleClickComplete = () => {
+    completeItemPress({item, index});
+  }
 
   return (
-    <View
-      style={{
-        backgroundColor,
-        padding: 30,
-        borderRadius: 10,
-        alignItems: 'center',
-      }}
-    >
-      <Text>{item.name}</Text>
-      <Text>{item.description}</Text>
+    <View style={stylesList.containerItemList}>
+      <View style={stylesList.textContainer}>
+        <Text>{item.name}</Text>
+        <Text>{item.description || 'Sem descrição'}</Text>
+      </View>
+
+      <View style={stylesList.actionContainer}>
+        <ActionButton icon="edit" title={"Editar"} onPress={handleClickEdit} />
+        <ActionButton icon="delete" title={"Excluir"} onPress={handleClickDelete} />
+        <ActionButton icon="check-circle" title={"Concluir"} onPress={handleClickComplete} />
+
+      </View>
     </View>
   );
 }
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -3,37 +3,90 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { Button, FlatList, Text, View } from "react-native";
 import ItemList from "./components/ItemList";
 import EmptyList from "./components/EmptyList";
-import InitialMessage from "./components/InitialMessage";
 import styles from "./styles";
-
-
-
-function Home({navigation}) {
-
+function Home() {
   const [items, setItems] = useState([]);
+  const [completedItems, setCompletedItems] = useState([]);
+  const [showCompletedItems, setShowCompletedItems] = useState(false);
+
+  const handleShowCompletedItems = () => {
+    setShowCompletedItems(!showCompletedItems);
+  }
+
+  const addItem = () => {
+    setItems([
+      ...items,
+      {
+        name: `item ${items.length + 1}`,
+        description: `Descrição do item ${items.length + 1}`,
+        done: false
+      }
+    ])
+  }
+
+  const completeItem = ({ item, index }) => {
+    // se estiver na lista normal
+    if (!item.done) {
+      // remove da lista normal
+      setItems(prevItems => prevItems.filter((_, i) => i !== index));
+
+      // adiciona nos concluídos
+      setCompletedItems(prevCompleted => [
+        ...prevCompleted,
+        { ...item, done: true }
+      ]);
+
+    } else {
+
+      // remove da lista de concluídos
+      setCompletedItems(prevCompleted =>
+        prevCompleted.filter((_, i) => i !== index)
+      );
+
+      // adiciona de volta na lista normal
+      setItems(prevItems => [
+        ...prevItems,
+        { ...item, done: false }
+      ]);
+    }
+  };
+
+   const removeItem = ({ item, index }) => {
+    // se estiver na lista normal
+    if (!item.done) {
+      // remove da lista normal
+      setItems(prevItems => prevItems.filter((_, i) => i !== index));
+    }
+
+    // remove da lista de concluídos
+      setCompletedItems(prevCompleted =>
+        prevCompleted.filter((_, i) => i !== index)
+      );
+  };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <InitialMessage navigation={navigation} />       
+    <SafeAreaView style={styles.container}>     
       <FlatList
-        data={items}
-        renderItem={ItemList}
+        keyExtractor={(item, index) => index.toString()}
+        data={showCompletedItems ? completedItems : items}
+        renderItem={({ item, index }) => <ItemList index={index} removeItemPress={removeItem} completeItemPress={completeItem} item={item} />}
         ListEmptyComponent={EmptyList}
         contentContainerStyle={styles.flatListContainer}
         ItemSeparatorComponent={() => (<View style={{height: 20}} />)}
       />
 
-      <Button
-        title="Clique para adicionar uma atividade"
-        onPress={() => setItems(
-        [...items,
-          { 
-            name: `item ${items.length + 1}`,
-            description: `Descrição do item ${items.length + 1}`,
-            done: false
-          }
-        ])}
-      />
+
+      <View style={{gap: 10}}> 
+        <Button
+          title={showCompletedItems ? "Mostrar todos" : "Mostrar itens concluídos"}
+          onPress={handleShowCompletedItems}
+        />
+
+        <Button
+          title="Clique para adicionar uma atividade"
+          onPress={addItem}
+        />
+      </View>
     </SafeAreaView>
   );
 }
